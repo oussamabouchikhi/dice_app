609f72e7d1ad526fa42720e0c62678de53962b89
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-02-13 20:04:55 +0100

    Adding initial project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Dice());
+
+class Dice extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+
+      home: null,
+    );
+  }
+}
+
+
